393196a552a98b1230a40a5601542a38b3cc5cd3
Emma Nami <149716579+emmanamii@users.noreply.github.com>
2025-03-03 19:33:18 +0900

    Added another function

--- Recursions/Functions.java
+++ Recursions/Functions.java
@@ -1,21 +1,18 @@
-package com.mycompany.recursions;
+package com.mycompany.recursions; 
 
-public class recursionsFunctions {
+public class Functions {
     
-    
-    public Integer strLength(String string){
+    public static Integer strLength(String string){
         //Manually creating a string.length
         if(string==null||string==""){
             return 0;
         }
         return 1+ strLength(string.substring(1));
                 //at the end: return 1+0;
-        
     }
     
-    public Integer factorial(Integer num){
+    public static Integer factorial(Integer num){
         //Input: 5 returns 120. 5*4*3*2*1 = 120
-
         if(num ==0){
             return 1;
         }
@@ -23,4 +20,12 @@ public class recursionsFunctions {
                 //return statement continnues until (num*factorial(num-1)) == 1: where this is called in the base case (if statement)
     }
     
+    public static Integer exponent(Integer num, Integer exp){
+        //Input: (2,4) or 2^4 will return 16
+        if(exp==0){
+            return 1;
+        }
+        return num* exponent(num, exp-1);
+    }   
 }
+
