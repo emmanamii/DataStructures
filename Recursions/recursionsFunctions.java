b645930b3437e86100da94b49b581658f01b1835
Emma Nami <149716579+emmanamii@users.noreply.github.com>
2025-03-03 18:42:04 +0900

    Update recursionsFunctions.java
    strLength();
    factorial();

--- Recursions/recursionsFunctions.java
+++ Recursions/recursionsFunctions.java
@@ -1,1 +1,26 @@
-//funcs
+package com.mycompany.recursions;
+
+public class recursionsFunctions {
+    
+    
+    public Integer strLength(String string){
+        //Manually creating a string.length
+        if(string==null||string==""){
+            return 0;
+        }
+        return 1+ strLength(string.substring(1));
+                //at the end: return 1+0;
+        
+    }
+    
+    public Integer factorial(Integer num){
+        //Input: 5 returns 120. 5*4*3*2*1 = 120
+
+        if(num ==0){
+            return 1;
+        }
+        return (num * factorial(num-1));
+                //return statement continnues until (num*factorial(num-1)) == 1: where this is called in the base case (if statement)
+    }
+    
+}
