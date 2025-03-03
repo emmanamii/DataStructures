41fbb9f6bedfd79b72a4993f99ac69ece7e67073
Emma Nami <149716579+emmanamii@users.noreply.github.com>
2025-03-04 01:34:06 +0900

    Update Parantheses.java

---
+++ Stacks/Parantheses.java
@@ -0,0 +1,42 @@
+package com.mycompany.palindrome;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Parantheses {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter equation:");
+        String string = scanner.next();
+        
+        System.out.println(hasBalancedParentheses(string));
+        
+    }
+
+    public static Boolean hasBalancedParentheses(String string) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < string.length(); i++) {
+            char value = string.charAt(i);
+
+            if (value == '(') {
+                stack.push(value);
+            }
+
+            else if (value == ')') {
+
+                if (stack.isEmpty()) {
+                    // If stack is empty, there is no matching '('
+                    return false;
+                }
+                // Pop the matching '(' from the stack
+                stack.pop();
+            }
+        }
+
+        // If stack is empty, parentheses are balanced; otherwise, they are not
+        return stack.isEmpty();
+    }
+}
