3b31880110952b741b0381f6a7b3c24e3cb151d0
Emma Nami <149716579+emmanamii@users.noreply.github.com>
2025-03-08 18:53:36 +0900

    Update functions.java

--- Recursions/functions.java
+++ Recursions/functions.java
@@ -1,7 +1,8 @@
-//Some recursive functions
+//Some recursive functions (practice)
 
-public class Functions {
-    
+public class Functions {  
+
+//------------------------------------------------------------------------
     public static Integer strLength(String string){
         //Manually creating a string.length
         if(string==null||string==""){
@@ -11,13 +12,42 @@ public class Functions {
                 //at the end: return 1+0;
     }
     
-    public static Integer gcd(Integer num1, Integer num2){
-        //Assume num1 > num2
-        if(num1%num2 == 0){
-            return num2;
+    public static boolean strPalindrome(String string){
+        //Return if palindrome is input
+        Integer length = string.length();
+        if(length<=1){
+            return true;
         }
-        return gcd(num2, num1%num2);
+        if(string.charAt(0)==string.charAt(length-1)){
+            String newString = string.substring(1, length-1);
+            return strPalindrome(newString);
+            
+        }
+        return false;
+        
     }    
+    
+    public static String strReversal(String string){
+        //Write a Java recursive method to reverse a given string.  
+        Integer length = string.length();
+        if(length ==1){
+            return string;
+        }
+        
+        String newString = string.substring(0, length-1);
+        return string.charAt(length-1) + strReversal(newString);
+    }  
+
+//------------------------------------------------------------------------
+    
+    public static Integer sum(Integer integer){
+        //Write a Java recursive method to calculate the sum of all numbers from 1 to n.
+        if(integer ==1){
+            return 1;
+        }
+       
+        return integer + sum(integer-1);
+    }     
     
     public static Integer factorial(Integer num){
         //Input: 5 returns 120. 5*4*3*2*1 = 120
@@ -27,6 +57,16 @@ public class Functions {
         return (num * factorial(num-1));
                 //return statement continnues until (num*factorial(num-1)) == 1: where this is called in the base case (if statement)
     }
+
+    //fibonacci one can go here. still working on it.
+    
+    public static Integer gcd(Integer num1, Integer num2){
+        //Assume num1 > num2
+        if(num1%num2 == 0){
+            return num2;
+        }
+        return gcd(num2, num1%num2);
+    }   
     
     public static Integer exponent(Integer num, Integer exp){
         //Input: (2,4) or 2^4 will return 16
