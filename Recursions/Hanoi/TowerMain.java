5d83444b401f3e07f2f7f0bc9b8317a892e1ce82
Emma Nami <149716579+emmanamii@users.noreply.github.com>
2025-03-09 23:30:04 +0900

    Create TowerMain.java

---
+++ Recursions/Hanoi/TowerMain.java
@@ -0,0 +1,32 @@
+import java.util.Stack;
+
+public class testing {
+
+    public static void main(String[] args) {
+
+        Stack<Integer> start = new Stack<>();   //Starting stack    #1
+        Stack<Integer> temp = new Stack<>();    //Temporary stack   #2
+        Stack<Integer> end = new Stack<>();     //Ending stack     #3
+        
+        Integer disc = 3;                      //Number of discs
+        
+        for(int i=disc;i>0;i--){               //Add the discs to the first stack
+            start.push(i);
+        }
+        
+        System.out.println("Before-----------------------------------");
+        TowerOfHanoi.printTower(start,temp,end);
+        System.out.println("-----------------------------------------");
+        
+        TowerOfHanoi.solveTower(disc, start, temp, end);
+
+        System.out.println("After------------------------------------");
+        TowerOfHanoi.printTower(start,temp,end);
+        System.out.println("-----------------------------------------");
+        
+        System.out.println("Moves used:" + TowerOfHanoi.calculateMoves(disc));
+
+    }
+
+
+}
