652fc12780b42d24a3203ce215362275c5b686b8
Emma Nami <149716579+emmanamii@users.noreply.github.com>
2025-03-29 08:05:35 +0900

    Update Node.java

--- Trees/BinaryTrees/Node.java
+++ Trees/BinaryTrees/Node.java
@@ -1,18 +1,13 @@
 //Node <E> is declared as an inner class within BinaryTree<E>
 //Node <E> is declared protected. This way we can use it as a superclass.
 
-protected static class Node<E> implements Serializable{
-  
-  protected E data;
-  protected Node<E> left;
-  protected Node<E> right;
-  
-  public Node(E data){
-    this.data = data;
-    left = null;
-    right = null;
-  }
-  public String toString(){
-    return data.toString();
-  }
+public class Node {
+    int data;
+    Node left, right;
+    
+    Node(int value){
+        data= value;
+        left = null;
+        right = null;
+    }
 }
