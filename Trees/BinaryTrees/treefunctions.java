28350d2bb2285b9cc9e0b03aed94f4232b523577
Emma Nami <149716579+emmanamii@users.noreply.github.com>
2025-03-30 18:56:29 +0900

    Update treefunctions.java

--- Trees/BinaryTrees/treefunctions.java
+++ Trees/BinaryTrees/treefunctions.java
@@ -1,18 +1,30 @@
 //Here are some functions for the different types of tree traversals
 
+/*Assume this class is used:
+public class Node {
+    int data;
+    Node left, right;
+    
+    Node(int value){
+        data= value;
+        left = null;
+        right = null;
+    }
+}
+*/
 public class treefunctions {
 
     public static void main(String[] args) {
-                                                /*--------------------------*/
-        Node root = new Node(11);               //  Tree:                   //   
-        root.left = new Node(2);                //               11         //            
-        root.right = new Node(29);              //            /    \        //         
-        root.left.left= new Node (1);           //           2      29      //        
-        root.left.right = new Node (7);         //         / \     /  \     //      
-        root.right.left = new Node (15);        //        1  7    15   40   //     
-        root.right.right = new Node(40);        //                    /     // 
-        root.right.right.left = new Node(35);   //                   35     //                          
-                                                /*--------------------------*/
+                                               
+        Node root = new Node(11);                
+        root.left = new Node(2);                   
+        root.right = new Node(29);                   
+        root.left.left= new Node (1);           
+        root.left.right = new Node (7);           
+        root.right.left = new Node (15);      
+        root.right.right = new Node(40);        
+        root.right.right.left = new Node(35);                       
+                                                
         System.out.print("PreOrder:  ");
         PreOrder(root);                
         System.out.println(" ");
